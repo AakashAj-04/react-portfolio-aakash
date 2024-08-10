623096dc01c33ec1c7a8a2ea7d261171461796b2
Aakash.A <Aakash.A@ril.com>
2024-08-10 22:37:08 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,75 @@
+import project1 from "/assets/projects/project_01.png";
+import project2 from "/assets/projects/project-02.jpg";
+import project3 from "/assets/projects/project_03.jpg";
+import project4 from "/assets/projects/project_04.jpg";
+
+export const HERO_CONTENT = `I am a passionate Frontend Software Developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React and Angular, as well as back-end technologies like Node.js, MySQL and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile Frontend Software Developer with a passion for creating efficient and user-friendly web applications. With 2 years of professional experience, I have worked with a variety of technologies, including React, Angular, Node.js. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Nov( 2023 )- Present",
+    role: "Software Developer",
+    company: "Jio Platform Limited",
+    description: `Developing and maintaining web applications using JavaScript, React.js . Integrated RESTful APIs with frontend components. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["React.js", "Next.js"],
+  },
+  {
+    year: " Jan( 2023 ) - Nov( 2023 )",
+    role: "Frontend Developer",
+    company: "Jio Platform Limited",
+    description: `Designed and developed user interfaces for web applications using Angular Material and PrimeNG. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Angular", "PrimeNG"],
+  },
+  {
+    year: " Sept( 2022 ) - Dec( 2022 )",
+    role: "Graduate Engineer Trainee",
+    company: "Jio Platform Limited",
+    description: `Maintained an Existing web applications using .Net and NodeJs .Implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: [".NET", "Csharp", "NodeJs"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Communication Telephony",
+    image: project1,
+    description:
+      "Communication Telephony is a platform to provide multiple services to customers. It is a multiple features available in media like sms, Campaign,etc.",
+    technologies: [
+      "HTML",
+      "SCSS",
+      "Angular",
+      "Angular Material",
+      "Custom-components",
+    ],
+  },
+  {
+    title: "Rich Communication Services (RCS)",
+    image: project2,
+    description:
+      "Rich Communication Services (RCS) is a communication protocol intended to replace SMS, offering a more feature-rich messaging experience.",
+    technologies: ["HTML", "SCSS", "Angular", "React.js", "Custom-components"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "MERN - CRUD ",
+    image: project4,
+    description:
+      "A project is used to perform some basic CRUD operations using Complete MERN Stack. It's my first full-stack project to implement my knowledge to work on those stacks",
+    technologies: ["MongoDB", "Express.js", "React.js", "Nodejs"],
+  },
+];
+
+export const CONTACT = {
+  address: "446/4, New Street, Pu.Mambakkam ,Tamlinadu - 606107 ",
+  phoneNo: "+91 9597145937",
+  email: "aakash04.amirthalingam@gmail.com",
+};
